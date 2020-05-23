adb4be3061f45a0636baed49e13ade37b7a37029
deian <deian@cs.ucsd.edu>
2020-05-23 18:42:28 +0000

    try other machine as master

--- forkWorkers.sh
+++ forkWorkers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-seq inf | parallel -j90 "swift run -c release FuzzilliCli --profile=spidermonkey ~/proton/fuzz-build/dist/bin/js --storagePath=./bugs --timeout=1160 --networkWorker=10.99.3.129:1337 &> worker.{}"
+seq inf | parallel -j90 "swift run -c release FuzzilliCli --profile=spidermonkey ~/proton/fuzz-build/dist/bin/js --storagePath=./bugs --timeout=1160 --networkWorker=10.99.3.135:1337 &> worker.{}"
 
--- master.sh
+++ master.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swift run -c release FuzzilliCli --profile=spidermonkey ../proton/fuzz-build/dist/bin/js --storagePath=./bugs --timeout=1160 --networkMaster=10.99.3.129:1337
+swift run -c release FuzzilliCli --profile=spidermonkey ../proton/fuzz-build/dist/bin/js --storagePath=./bugs --timeout=1160 --networkMaster=10.99.3.135:1337
