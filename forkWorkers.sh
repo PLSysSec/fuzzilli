2069c507d1d247f876872cf8844060ac5ea2fe40
deian <deian@cs.ucsd.edu>
2020-05-24 01:04:27 +0000

    fuck your shit

--- forkWorkers.sh
+++ forkWorkers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-seq inf | parallel -j96 "swift run -c release FuzzilliCli --profile=spidermonkey ~/proton/fuzz-build/dist/bin/js --storagePath=../bugs --timeout=1160 --networkWorker=10.99.3.135:1337 &> /tmp/worker.{}"
+seq inf | parallel -j96 "swift run -c release FuzzilliCli --profile=spidermonkey ~/proton/fuzz-build/dist/bin/js --storagePath=/home/d/bugs --timeout=1160 --networkWorker=127.0.01:1337 &> /tmp/worker.{}"
